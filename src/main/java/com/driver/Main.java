0919a2dfc6e50043bbf763248e03ba54719b9207
Ankitakamthe153 <88307915+Ankitakamthe153@users.noreply.github.com>
2023-12-11 21:04:01 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,16 +1,17 @@
 package com.driver;
 
 public class Main {
-        public int product(int x, int y) {
-            return x*y;
-        }
-        public int product(int x, int y, int z) {
-            return x*y*z;
-        }
-        public double product(double x, double y) {
-            return x*y;
-        }
-    }
+        public static class Product{
+           public int product(int x, int y) {
+               return x*y;
+           }
+           public int product(int x, int y, int z) {
+               return x*y*z;
+          }
+           public double product(double x, double y) {
+              return x*y;
+          }
+     }
     public static void main(String args[]){
         Product p = new Product();//creating object of product class
         p.product(0,0);
